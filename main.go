673613bccc2ba0211c083f9b2ff29a4885ca2aef
Sumit Jadhav (Thouhtworks, IT, KLI) <kli.sumit-jadhav@kotak.com>
2024-04-16 15:48:44 +0530

    Add wait group to halt execution till all goroutines complete

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func printCharacter(c string, times int) {
+func printCharacter(c string, times int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := 0; i < times; i++ {
 		fmt.Println(c)
 		time.Sleep(100 * time.Millisecond)
@@ -14,9 +16,14 @@ func printCharacter(c string, times int) {
 
 func main() {
 	fmt.Println("Application started.")
-	go printCharacter("A", 10)
-	go printCharacter("B", 10)
-	go printCharacter("C", 10)
-	time.Sleep(2 * time.Second)
 	defer fmt.Println("Application ended.")
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go printCharacter("A", 10, &wg)
+	go printCharacter("B", 10, &wg)
+	go printCharacter("C", 10, &wg)
+
+	wg.Wait()
 }
